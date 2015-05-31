9a22a529461cc55c9c680e7f7f0c7e68697aa3e7
Mila <mila@apiary.io>
2015-05-31 19:39:34 +0200

    car semaphore

--- YPP2/Votradovec_03.lisp
+++ YPP2/Votradovec_03.lisp
@@ -14,39 +14,83 @@
     x y)
    radius))
 
-(defun make-semaphore-lights ()
-  (list (make-light 90 55 30 :red)
-        (make-light 90 125 30 :green)))
-
-(defmethod initialize-instance ((s semaphore) &rest intiargs)
-  (call-next-method)
-  (setf (slot-value s 'lights) (make-semaphore-lights))
-  (set-items s (slot-value s 'lights)))
+(defun make-semaphore-lights (count)
+  (if (= count 2)
+      (list (make-light 90 55 30 :red)
+            (make-light 90 125 30 :green))
+    (list (make-light 90 55 30 :red)
+          (make-light 90 125 30 :orange)
+          (make-light 90 195 30 :green))))
         
 (defmethod light-count ((s semaphore))
   (slot-value s 'light-count))
 
 (defmethod set-light-count ((s semaphore) count)
   (check-light-count s count)
-  (setf (slot-value s 'light-count) count)
-  (set-initial-phase s))
+  (setf (slot-value s 'lights) (make-instance 'car-semaphore))
+  (set-items s (list (slot-value s 'lights))))
 
 (defmethod check-light-count ((s semaphore) count)
   (if (or (< count 2) (> count 3))
       (error "Light count must be 2 or 3.")))
 
 (defmethod next-phase ((s semaphore))
-  (if (= 2 (light-count s))
-      (send-to-items s #'toggle)))
+ (apply #'next-phase (slot-value s 'lights) '()))
 
 (defmethod set-initial-phase ((s semaphore))
   (set-on (nth 0 (items s)))
-  (set-off (nth 1 (items s)))))
+  (set-off (nth 1 (items s))))
 
-;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;PEDESTRIAN SEMAPHORE CLASS;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defclass pedestrian-semaphore (picture)
+  ())
+
+(defmethod next-phase ((ps pedestrian-semaphore))
+   (send-to-items ps #'toggle))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;CAR SEMAPHORE LIGHTS;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+(defclass car-semaphore (picture)
+  ((current-phase :initform 0)
+   (lights :initform nil)))
+
+(defmethod initialize-instance ((cs car-semaphore) &key)
+  (call-next-method)
+  (set-items cs (make-semaphore-lights 3)))
+
+(defmethod set-current-phase ((cs car-semaphore) phase)
+  (setf (slot-value cs 'current-phase) phase))
+
+(defmethod current-phase ((cs car-semaphore))
+  (slot-value cs 'current-phase))
+
+(defmethod next-phase ((cs car-semaphore))
+    (if (= 3 (current-phase cs))
+        (set-current-phase cs 0)
+        (set-current-phase cs (+ (current-phase cs) 1)))
+   (let ((current-phase (current-phase cs)))
+    (cond ((= 0 current-phase) 
+           (setup-lights cs (list #'set-on #'set-off #'set-off)))
+          ((= 1 current-phase) 
+           (setup-lights cs (list #'set-on #'set-on #'set-off)))
+          ((= 2 current-phase) 
+           (setup-lights cs (list #'set-off #'set-off #'set-on)))
+          ((= 3 current-phase) 
+           (setup-lights cs (list #'set-off #'set-on #'set-off))))))
+
+(defmethod setup-lights ((cs car-semaphore) setup-list)
+  (mapcar (lambda (light setup)
+            (apply setup light '()))
+          (items cs)
+          setup-list))
+
+;; TESTS
 (setf s (make-instance 'semaphore))
 (setf w (make-instance 'window))
 (set-shape w s)
-(set-light-count s 2)
+(set-light-count s 3)
 (redraw w)
 (next-phase s)
