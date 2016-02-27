492a9deb0595ecef73523f40033d20b91064326e
Mila <mila@apiary.io>
2016-02-27 19:16:13 +0100

    refactor validation

--- YPS1/Evade/src/Referee.java
+++ YPS1/Evade/src/Referee.java
@@ -17,53 +17,14 @@ public class Referee {
     public boolean validateTurn(Turn turn, Player player) {
         Position from = turn.getFrom();
         Position to = turn.getTo();
-
-        // Make turns on board only
-        if (!isOnBoard(from) || !isOnBoard(to)) {
-            return false;
-        }
-
-        if (from == null) {
-            return false;
-        }
-
-        // Don't go to the original position
-        int columnDiff = from.getColumn() - to.getColumn();
-        if (Math.abs(columnDiff) > 1 ) {
-            return false;
-        }
-
-        int rowDiff = from.getRow() - to.getRow();
-        if (Math.abs(rowDiff) > 1 ) {
-            return false;
-        }
-
-        if (rowDiff == 0 && columnDiff == 0) {
-            return false;
-        }
-
-        //Do not move frozen pieces
-        Deck deck = board.getDeck();
-        if((deck.getPositionValue(from) & Deck.FROZEN) == Deck.FROZEN) {
-            return false;
-        }
-
-        //Move only own pieces
-        if((deck.getPositionValue(from) & player.getSign()) == 0) {
-            return false;
-        }
-
-        //Don't freeze your own piece
-        if((deck.getPositionValue(to) & player.getSign()) > 0) {
-            return false;
-        }
-
-        //Don't let king freeze anybody
-        if (((deck.getPositionValue(from) & Deck.KING) > 0) && (deck.getPositionValue(to) != Deck.EMPTY)) {
-            return false;
-        }
-
-        return true;
+        
+        return from != null &&
+                this.isOnBoard(from) &&
+                this.isOnBoard(to) &&
+                this.isTargetDifferent(from, to) &&
+                this.isNotFrozen(from) &&
+                this.isOwn(from, player) &&
+                this.canFreeze(from, to, player);
     }
 
     public ArrayList <Turn> getValidTurnsForPlayer(Player player) {
@@ -95,6 +56,7 @@ public class Referee {
         }
 
         //If opponents king is on the winning side, current player loses
+        //FIXME: Frozen king is handled as a winning position
         int [][] board = this.board.getDeck().getDeck();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
@@ -113,5 +75,53 @@ public class Referee {
     private boolean isOnBoard(Position position) {
         return position.getColumn() >= 0 && position.getColumn() < 6 &&
                 position.getRow() >= 0 && position.getRow() < 6;
+    }
+
+    private boolean isTargetDifferent(Position from, Position to) {
+        int columnDiff = from.getColumn() - to.getColumn();
+        if (Math.abs(columnDiff) > 1 ) {
+            return false;
+        }
+
+        int rowDiff = from.getRow() - to.getRow();
+        if (Math.abs(rowDiff) > 1 ) {
+            return false;
+        }
+
+        if (rowDiff == 0 && columnDiff == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isNotFrozen(Position from) {
+        Deck deck = board.getDeck();
+        if((deck.getPositionValue(from) & Deck.FROZEN) == Deck.FROZEN) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isOwn(Position from, Player player) {
+        Deck deck = board.getDeck();
+        if((deck.getPositionValue(from) & player.getSign()) == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean canFreeze(Position from, Position to, Player player) {
+        Deck deck = board.getDeck();
+
+        if(deck.getPositionValue(to) != Deck.EMPTY &&
+                (((deck.getPositionValue(to) & player.getSign()) > 0 ) ||
+                ((deck.getPositionValue(from) & Deck.KING) > 0))) {
+            return false;
+        }
+
+        return true;
     }
 }
