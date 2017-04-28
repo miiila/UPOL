76b8a1a8b82e325bde8dfe122c5a35f6ae774fb6
miiila <mila.votradovec@gmail.com>
2017-04-28 18:48:16 +0200

    add filter component

--- YINFS/yinfs/jennifer/static/jenn.js
+++ YINFS/yinfs/jennifer/static/jenn.js
@@ -10,23 +10,49 @@ Vue.component('page-content', {
     template: '<section class="section--text"><p><slot></slot></p></section>'
 });
 
-Vue.component('publications', {
-    template: '<ul><slot></slot></ul>'
-});
-
-Vue.component('publication', {
-    props: ['name', 'type', 'abstract', 'authors', 'pubdata'],
-    template: '<li>{{ name }}</li>'
-});
-
-var app = new Vue({
-        el: '.app',
-        data: {
-            currentRoute: window.location.hash || '#about-me',
-        },
-        methods: {
-          addPublication: (item) => this.publications.push(item)
+Vue.component('filtered-list', {
+    props: ['items', 'filteroptions', 'filterkey'],
+    data() {return {selectedTypes: [allTypes]}},
+    template: `<div>
+                <filter-inputs @update="selectedTypes = $event" :items="filteroptions" :selectedTypes="selectedTypes" ></filter-inputs>
+                    <ul>
+                        <filter-item v-for="item in items" :item="item" :key="item.name" v-show="toShow(item[filterkey])"></filter-item>
+                    </ul>
+                </div>`,
+    methods: {
+        toShow(type) {
+            return this.selectedTypes.indexOf(allTypes) > -1 || this.selectedTypes.indexOf(type) > -1;
         }
+    }
+});
+
+Vue.component('filter-item', {
+    props: ['item'],
+    template: '<li>{{item.type}}: {{ item.name }}</li>'
+});
+
+Vue.component('filter-inputs', {
+    props: ['items'] ,
+    data() {return {selected: []}},
+    methods: {
+        change() {this.$emit('update', this.selected)}
+    },
+    template: `<form>
+                <template v-for="item in items">
+                    <input type="radio" :id="item" :value="item" v-model="selected" @click="change()"><label :for="item">{{item}}</label>
+                </template>
+               </form>`
+});
+
+const allTypes = 'All types';
+
+const app = new Vue({
+    el: '.app',
+    data: {
+        currentRoute: window.location.hash || '#about-me',
+        publications: [],
+        publicationTypes: [allTypes]
+    }
 });
 
 window.addEventListener('popstate', () => {
--- YINFS/yinfs/jennifer/views.py
+++ YINFS/yinfs/jennifer/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .models import Section, Publication
+from .models import Section, Publication, PublicationType
 
 
 def index(request):
     context = {
         'title': 'Jennifer Doe - homepage',
         'menuItems': Section.objects.all(),
-        'publications': Publication.objects.all()
+        'publications': Publication.objects.all(),
+        'types': PublicationType.objects.all()
     }
 
     return render(request, 'index.html', context)
