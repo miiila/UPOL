f9b5d1ec3c4fa49986fc7b818f05d5f909ebd322
miiila <mila.votradovec@gmail.com>
2017-04-28 12:13:22 +0200

    skeleton of publication filter

--- YINFS/yinfs/jennifer/static/jenn.js
+++ YINFS/yinfs/jennifer/static/jenn.js
@@ -10,11 +10,22 @@ Vue.component('page-content', {
     template: '<section class="section--text"><p><slot></slot></p></section>'
 });
 
+Vue.component('publications', {
+    template: '<ul><slot></slot></ul>'
+});
+
+Vue.component('publication', {
+    props: ['name', 'type', 'abstract', 'authors', 'pubdata'],
+    template: '<li>{{ name }}</li>'
+});
 
 var app = new Vue({
         el: '.app',
         data: {
-            currentRoute: window.location.hash || '#about-me'
+            currentRoute: window.location.hash || '#about-me',
+        },
+        methods: {
+          addPublication: (item) => this.publications.push(item)
         }
 });
 
--- YINFS/yinfs/jennifer/views.py
+++ YINFS/yinfs/jennifer/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .models import Section
+from .models import Section, Publication
 
 
 def index(request):
     context = {
         'title': 'Jennifer Doe - homepage',
         'menuItems': Section.objects.all(),
+        'publications': Publication.objects.all()
     }
 
     return render(request, 'index.html', context)
