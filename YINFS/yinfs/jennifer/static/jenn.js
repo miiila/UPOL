63cdea2e4c41c760c6f18f3e3bf0dff5e8b983c2
miiila <mila.votradovec@gmail.com>
2017-04-29 12:03:22 +0200

    publication filter code

--- YINFS/yinfs/jennifer/static/jenn.js
+++ YINFS/yinfs/jennifer/static/jenn.js
@@ -11,17 +11,18 @@ Vue.component('page-content', {
 });
 
 Vue.component('filtered-list', {
-    props: ['items', 'filteroptions', 'filterkey'],
-    data() {return {selectedTypes: [allTypes]}},
-    template: `<div>
-                <filter-inputs @update="selectedTypes = $event" :items="filteroptions" :selectedTypes="selectedTypes" ></filter-inputs>
-                    <ul>
+    props: ['items', 'filteroptions', 'filterkey', 'title'],
+    data() {return {selectedType: allTypes}},
+    template: `<div class="filter">
+                <span class="filter__title">{{ title }}</span>
+                <filter-inputs @update="selectedType = $event" :items="filteroptions" :selectedType="selectedType" ></filter-inputs>
+                    <ul class="filter__content">
                         <filter-item v-for="item in items" :item="item" :key="item.name" v-show="toShow(item[filterkey])"></filter-item>
                     </ul>
                 </div>`,
     methods: {
         toShow(type) {
-            return this.selectedTypes.indexOf(allTypes) > -1 || this.selectedTypes.indexOf(type) > -1;
+            return this.selectedType == allTypes || this.selectedType == type;
         }
     }
 });
@@ -33,15 +34,15 @@ Vue.component('filter-item', {
 
 Vue.component('filter-inputs', {
     props: ['items'] ,
-    data() {return {selected: []}},
+    data() {return {selected: allTypes}},
     methods: {
         change() {this.$emit('update', this.selected)}
     },
-    template: `<form>
-                <template v-for="item in items">
-                    <input type="radio" :id="item" :value="item" v-model="selected" @click="change()"><label :for="item">{{item}}</label>
-                </template>
-               </form>`
+    template: `<select class="filter__select" v-model="selected" @change="change()">
+                    <template v-for="item in items">
+                            <option :value="item">{{item}}</option>
+                    </template>
+               </select>`
 });
 
 const allTypes = 'All types';
