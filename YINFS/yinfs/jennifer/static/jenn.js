f2feb596d1d2afb20efc0b258a04e7c4a6af5458
miiila <mila.votradovec@gmail.com>
2017-04-30 16:29:02 +0200

    style, filter, hide etc.

--- YINFS/yinfs/jennifer/static/jenn.js
+++ YINFS/yinfs/jennifer/static/jenn.js
@@ -1,3 +1,7 @@
+timePeriodSort = (a, b) => {
+    return (b.yearStart - a.yearStart);
+};
+
 Vue.component('menu-item', {
     props: ['item'],
     template: '<a v-bind:href="link" class="nav--item">{{ item }}</a>',
@@ -22,7 +26,7 @@ Vue.component('filtered-list', {
                 </div>`,
     methods: {
         toShow(type) {
-            return this.selectedType == allTypes || this.selectedType == type;
+            return this.selectedType === allTypes || this.selectedType === type;
         }
     }
 });
@@ -45,6 +49,128 @@ Vue.component('filter-inputs', {
                </select>`
 });
 
+Vue.component('teaching', {
+    props: ['courses'],
+    data() {
+        return {
+            details: false
+        }
+    },
+    template:`
+        <div class="teaching">
+            <div class="timeline">
+                <span class="timeline__title">Current teaching</span>
+                <hr>
+                <table>
+                    <teaching-details v-for="item in activeCourses" :item="item"> 
+                    </teaching-details>
+                </table>
+            </div>
+            <div class="timeline">
+                <span class="timeline__title">Teaching History</span>
+                <hr>
+                <table>
+                    <timeline-name-with-details v-for="item in inactiveCourses" :item="item" :past="true"> 
+                    </timeline-name-with-details>
+                </table>
+            </div>
+        </div>
+    `,
+    computed: {
+      activeCourses() {
+          return this.courses.filter((course) => {return course.active}).sort(timePeriodSort);
+      },
+      inactiveCourses() {
+          return this.courses.filter((course) => {return !course.active}).sort(timePeriodSort);
+      }
+    },
+});
+
+Vue.component('timeline-name-with-details', {
+    props: ['item', 'past'],
+    data() {
+        return {
+            showDetails: false
+        }
+    },
+    template:`
+        <tr class="timeline__row">
+            <td class="timeline__period" :class="{'timeline__period--passsive': this.past}"> {{ item.yearStart }} - {{ item.yearEnd || "present" }}</td>
+            <td class="teaching__name"><span class="timeline__details" :class="{'timeline__details--hidden': !this.showDetails, 'timeline__details--shown': this.showDetails}" @click="showDetails=!showDetails">{{ item.name }}</span><span class="teaching__details" v-show="showDetails"><br/>{{ item.description }}</span> </td>
+        </tr>  
+    `
+});
+
+Vue.component('education', {
+    props: ['items'],
+    computed: {
+        sorted() {
+            return this.items.sort(timePeriodSort)
+        }
+    },
+    template:`
+        <div class="education">
+            <span class="timeline__title">Education</span>
+            <hr>
+            <table>
+                <tr v-for="item in sorted" class="timeline__row">
+                    <td class="timeline__period"> {{ item.graduationYear }}</td>
+                    <td class="positions__name">{{ item.name }}<br /> <span class="positions__location" >{{ item.university }}, {{ item.faculty }}</span></td> 
+                </tr>
+            </table>
+        </div>
+    `
+});
+
+Vue.component('positions', {
+    props: ['items'],
+    computed: {
+        sorted() {
+            return this.items.sort(timePeriodSort)
+        }
+    },
+    template:`
+        <div class="timeline">
+            <span class="timeline__title">Academic Positions</span>
+            <hr>
+            <table>
+                <tr v-for="item in sorted" class="timeline__row">
+                    <td class="timeline__period"> {{ item.yearStart }} - {{ item.yearEnd || "present" }}</td>
+                    <td class="positions__name">{{ item.name }}<br /> <span class="positions__location" >{{ item.university }}</span></td> 
+                </tr>
+            </table>
+        </div>
+    `
+});
+
+Vue.component('awards', {
+    props: ['items'],
+    computed: {
+        sorted() {
+            return this.items.sort(timePeriodSort)
+        }
+    },
+    methods: {
+        getYearString(year) {
+            if (year) {
+                return " - " + year;
+            } else {
+                return "";
+            }
+        }
+    },
+    template:`
+        <div class="timeline">
+            <span class="timeline__title">Honors, Awards and Grants</span>
+            <hr>
+            <table>
+                    <timeline-name-with-details v-for="item in sorted" :item="item"> 
+                    </timeline-name-with-details>
+            </table>
+        </div>
+    `
+});
+
 const allTypes = 'All types';
 
 const app = new Vue({
@@ -53,7 +179,10 @@ const app = new Vue({
         currentRoute: window.location.hash || '#about-me',
         publications: [],
         publicationTypes: [allTypes],
-        educations: []
+        teachingClasses: [],
+        positions: [],
+        education: [],
+        awards: []
     }
 });
 
